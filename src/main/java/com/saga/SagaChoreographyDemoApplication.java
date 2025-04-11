52a40908583fbb35f1ac08c837d41e011a41ff93
Serhii Shapoval <acidserg359@gmail.com>
2025-04-11 22:31:20 +0300

    Initial commit

---
+++ src/main/java/com/saga/SagaChoreographyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaChoreographyDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SagaChoreographyDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/saga/SagaChoreographyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaChoreographyDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
